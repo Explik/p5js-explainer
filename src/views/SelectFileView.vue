883b2209cae13efe81dcfbe2cc743f4234cef548
Explik <90565470+Explik@users.noreply.github.com>
2024-10-16 19:00:16 +0200

    Centering file selection component

--- src/views/SelectFileView.vue
+++ src/views/SelectFileView.vue
@@ -1,32 +1,33 @@
 <template>
-    <v-container>
-        <v-card>
-            <v-card-title>
-                <v-icon left class="mr-1">mdi-robot-happy</v-icon>
-                Vælg en fil!
-            </v-card-title>
+    <v-container class="fill-height">
+        <v-responsive>
+            <v-row>
+                <v-col cols="8" class="mx-auto">
+                    <v-card style="height: 400px;">
+                        <v-card-title>
+                            <v-icon left class="mr-1">mdi-robot-happy</v-icon>
+                            Vælg en fil!
+                        </v-card-title>
 
-            <v-divider></v-divider>
+                        <v-divider></v-divider>
 
-            <v-list v-model:opened="open">
-                <v-list-group 
-                    v-for="category in categories"
-                    :value="category.id">
-                    <template v-slot:activator="{ props }">
-                        <v-list-item v-bind="props" prepend-icon="mdi-folder" :title="category.name"></v-list-item>
-                    </template>
+                        <v-list v-model:opened="open">
+                            <v-list-group v-for="category in categories" :value="category.id">
+                                <template v-slot:activator="{ props }">
+                                    <v-list-item v-bind="props" prepend-icon="mdi-folder"
+                                        :title="category.name"></v-list-item>
+                                </template>
 
-                    <v-list-item 
-                        v-for="item in category.items" 
-                        :key="item.name" 
-                        prepend-icon="mdi-file" 
-                        :title="item.name"
-                        :value="title"
-                        @click-once="() => handleSelection(item)">
-                    </v-list-item>
-                </v-list-group>
-            </v-list>
-        </v-card>
+                                <v-list-item v-for="item in category.items" :key="item.name" prepend-icon="mdi-file"
+                                    :title="item.name" :value="title" @click-once="() => handleSelection(item)">
+                                </v-list-item>
+                            </v-list-group>
+                        </v-list>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-responsive>
+
     </v-container>
 </template>
 
@@ -35,16 +36,16 @@ export default {
     data: () => ({
         open: [],
         categories: [
-            { 
-                id: "category-1", 
-                name: "Category 1", 
-                icon: "mdi-weight-bathroom", 
+            {
+                id: "category-1",
+                name: "Category 1",
+                icon: "mdi-weight-bathroom",
                 items: [
                     { name: "BMI Calculator", icon: "mdi-plus", path: "bmi-calculator" },
                     { name: "Item #2", icon: "mdi-eye", path: "" },
                     { name: "Item #3", icon: "mdi-pencil", path: "" },
                     { name: "Item #4", icon: "mdi-delete", path: "" },
-                ] 
+                ]
             },
         ],
     }),
