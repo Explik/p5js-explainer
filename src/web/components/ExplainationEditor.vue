5162f9cd49686b383680f48a4a1278a42e3db922
Explik <90565470+Explik@users.noreply.github.com>
2024-10-25 15:46:57 +0200

    Reworking edit layout and adding redirect on final save

--- src/web/components/ExplainationEditor.vue
+++ src/web/components/ExplainationEditor.vue
@@ -1,17 +1,13 @@
 <template>
     <v-card class="px-6 py-6 overflow-y-scroll">
-        <v-tabs 
-            v-model="tab"
-            color="primary"
-            direction="vertical">
-            <v-tab text="#1 - Indhold" value="code"></v-tab>
-            <v-tab text="#2 Forklaringer" value="comments"></v-tab>
-            <v-tab text="#3 - Referencer" value="references"></v-tab>
+        <h1>{{ tabTitle }}</h1>
+
+        <v-tabs v-model="currentTab" color="primary" direction="vertical" class="mb-4">
+            <v-tab v-for="tab in tabs" :text="tab.label" :value="tab.name"></v-tab>
         </v-tabs>
 
-        <v-tabs-window v-model="tab">
+        <v-tabs-window v-model="currentTab">
             <v-tabs-window-item value="code" class="p4">
-                <h1>Indhold</h1>
                 <v-text-field v-model="fileName" label="Fil navn" required></v-text-field>
 
                 <CodeEditor 
@@ -21,13 +17,11 @@
                 </CodeEditor>
                 
                 <div class="float-right">
-                    <v-btn @click="handleRangesUpdate()">Gem og videre</v-btn>
+                    <v-btn @click="handleTabChange(1)">Gem og videre</v-btn>
                 </div>
             </v-tabs-window-item>
 
             <v-tabs-window-item value="comments">
-                <h1>Forklaringer</h1>
-
                 <div v-if="codeComments.length"  v-for="(comment, index) in codeComments" :key="index" class="mb-4">
                     <v-card :class="{updated: comment.updated}">
                         <v-card-text>
@@ -44,13 +38,11 @@
                 </div>
 
                 <div class="float-right">
-                    <v-btn @click="handleCommentsUpdate()">Gem og videre</v-btn>
+                    <v-btn @click="handleTabChange(2)">Gem og videre</v-btn>
                 </div>
             </v-tabs-window-item>
 
             <v-tabs-window-item value="references">
-                <h1>Referencer</h1>
-
                 <div v-if="codeReferences.length" v-for="(referenceGroup, index) in codeReferences" :key="index" class="mb-4">
                     <v-card>
                         <v-card-text>
@@ -72,7 +64,7 @@
                 </div>
 
                 <div class="float-right">
-                    <v-btn @click="handleReferencesUpdate()">Gem og videre</v-btn>
+                    <v-btn @click="handleTabChange(3)">Gem</v-btn>
                 </div>
             </v-tabs-window-item>
         </v-tabs-window>
@@ -126,38 +118,56 @@ export default {
         //debugger;
         return {
             step: 1,
-            tab: undefined,
+            currentTab: 0,
+            tabs: [
+                { id: "code", label: "Trin 1 - Indhold", title: "Indhold" },
+                { id: "comments", label: "Trin 2A - Forklaringer", title: "Forklaringer" },
+                { id: "references", label: "Trin 2B - Referencer", title: "Referencer" }
+            ],
             fileName: "",
             fileContent: this.code,
             isLoading: true,
         };
     },
     computed: {
-        subtotal() {
-            return this.products.reduce((acc, product) => acc + product.quantity * product.price, 0)
+        tabTitle() {
+            return this.tabs[this.currentTab]?.title ?? "[tabTitle]";
         },
-        total() {
-            return this.subtotal + Number(this.shipping ?? 0)
-        },
+        isCurrentTabLocked() {
+            return this.currentTab === 0 && !this.code.length && !this.codeRanges.length;
+        }
     },
     methods: {
         handleCodeUpdate() {
             this.$emit('file-content-update', this.fileContent);
         },
-        handleRangesUpdate() {
-            this.$emit('file-ranges-update', this.codeRanges);
-            this.tab = "comments";
-        },
-        handleCommentsUpdate() {
-            this.$emit('file-comments-update', this.codeComments);
-            this.tab = "references";  
-        },
-        handleReferencesUpdate() {
-            this.$emit('file-references-update', this.codeReferences);
-        },
         handleReferenceDeleted(referenceGroup, reference) {
             referenceGroup.references = referenceGroup.references.filter(r => r !== reference);
             referenceGroup.updated = true;
+        },
+        handleTabChange(newCurrentTab) {
+            if (this.currentTab === newCurrentTab)
+                return;
+            if (this.isCurrentTabLocked) 
+                return;
+            
+            if (newCurrentTab === 0) {
+                // Do nothing
+            }
+            else if (newCurrentTab === 1) {
+                this.$emit('file-ranges-update', this.codeRanges);
+                this.currentTab = newCurrentTab;
+            }
+            else if (newCurrentTab === 2) {
+                this.$emit('file-comments-update', this.codeComments);
+                this.currentTab = newCurrentTab;
+            }
+            else if (newCurrentTab === 3) {
+                this.$emit('file-references-update', this.codeReferences);
+                this.$router.push({ name: 'explain', params: { id: this.$route.params.id } });
+                return;
+            }
+            else throw new Error("Invalid tab index");
         }
     }
 }
